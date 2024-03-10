4acaf3ec4fd1b713aeb5fc99945fff00a30ab9f7
Mahmoud-Elbatran <138718611+Mahmoud-Elbatran@users.noreply.github.com>
2024-03-10 22:36:22 +0200

    Add files via upload

---
+++ Stock Portfolio Tracker.py
@@ -0,0 +1,5 @@
+# Stock Portfolio Tracker
+
+# Create a stock portfolio tracking tool that allows users
+# to add, remove, and track the performance of their
+# stock investments. Utilize financial APIs for real-time stock data.
